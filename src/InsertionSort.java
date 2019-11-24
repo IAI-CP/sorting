e9e251f4c5cf951a9025248a80f1ea81e1eee2fa
Adel <ibragimov.adel.kzn@gmail.com>
2019-11-24 15:18:43 +0300

    created insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -4,4 +4,19 @@ public class InsertionSort {
 
     }
 
+    public static void sort(int[] a) {
+        int j, n;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                j = i;
+                n = a[i + 1];
+                while (j >= 1 && n < a[j]) {
+                    a[j + 1] = a[j];
+                    a[j] = n;
+                    j--;
+                }
+            }
+        }
+    }
+
 }
