75f65ff3803ca17ac0b41e42ba6ec9e467c5a4cd
Adel <ibragimov.adel.kzn@gmail.com>
2019-12-02 08:39:21 +0300

    deleted main

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,13 +1,5 @@
 public class InsertionSort {
 
-    public static void main(String[] args) {
-        int[] a = {5, 4, 3, 2, 1};
-        sort(a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
-    }
-
     public static void sort(int[] a) {
         int j, n;
         for (int i = 0; i < a.length - 1; i++) {
