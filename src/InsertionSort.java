c992c8ef54c15d07cd4376cb241e9e696621224f
Adel <ibragimov.adel.kzn@gmail.com>
2019-12-02 08:37:35 +0300

    sort fix

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,7 +1,11 @@
 public class InsertionSort {
 
     public static void main(String[] args) {
-
+        int[] a = {5, 4, 3, 2, 1};
+        sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 
     public static void sort(int[] a) {
@@ -10,7 +14,7 @@ public class InsertionSort {
             if (a[i] > a[i + 1]) {
                 j = i;
                 n = a[i + 1];
-                while (j >= 1 && n < a[j]) {
+                while (j >= 0 && n < a[j]) {
                     a[j + 1] = a[j];
                     a[j] = n;
                     j--;
